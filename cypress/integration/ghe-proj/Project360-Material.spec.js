62949a75c0c1e07ea979754132a1a7b75def1808
masz77 <duong.buinhat1809@gmail.com>
2022-04-26 01:17:47 +0700

    further optimize material tab

--- cypress/integration/ghe-proj/Project360-Material.spec.js
+++ cypress/integration/ghe-proj/Project360-Material.spec.js
@@ -5,10 +5,15 @@ describe('project360 - material tab functionalities', () => {
     beforeEach('add api listener', () => {
         cy.intercept('POST', '/api/v1/material').as('matSaveStatus')
         cy.intercept('DELETE', '/api/v1/material').as('matDeleteStatus')
-
+        //on failed event
+        // cy.on('fail', (e) => {
+        //   console.error(e)
+        // })
     })
+
     context('creates new project with details', () => {
-      it('add new', () => {
+      it('navigate to material -> add new material', () => {
+
         //log in and assert
         cy.visit(Cypress.config().baseUrl)
         cy.logInCmd('admin', 'admin').type('{enter}')
@@ -21,18 +26,30 @@ describe('project360 - material tab functionalities', () => {
         //click add new
         cy.get('a[data-test-id="addNewBtn"]').click()
         cy.get('div').contains('New material').should('be.visible')
+      })
+
+      it('save button when no data entered', () => {
 
         //try save button when no data entered
         cy.get('button[data-test-id="saveBtn"]').click()
         cy.get('input[name="number"]').should('have.attr','aria-invalid','true')
         cy.get('input[name="name"]').should('have.attr','aria-invalid','true')
+      })
+
+      it('reset button', () => {
 
         //try reset button
         cy.fillInMaterialDetail('1','1','1')
         cy.get('button[data-test-id="reset"]').click()
         //assert empty
-        cy.get('input[name="number"]').should('be.empty')
-        cy.get('input[name="name"]').should('be.empty')
+        cy.get('input[name="number"]').invoke('val').should('be.empty')
+        // cy.get('input[name="name"]').should('be.empty')
+        // cy.get('input[name="brand"]').should('be.empty')
+        cy.get('input[name="name"]').invoke('val').should('be.empty')
+        cy.get('input[name="brand"]').invoke('val').should('be.empty')
+      })
+
+      it('save button', () => {
 
         //try save button
         cy.fillInMaterialDetail('test-number', 'test-name', 'test-brand')
