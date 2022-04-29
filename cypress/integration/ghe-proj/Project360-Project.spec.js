993b44e7158c62b31a5155dc65807b746c92ba31
duong <duong.bui@opswaat.com>
2022-04-29 15:32:50 +0700

    project add new mat case

--- cypress/integration/ghe-proj/Project360-Project.spec.js
+++ cypress/integration/ghe-proj/Project360-Project.spec.js
@@ -5,9 +5,6 @@ describe('project360 - project tab functionalities', () => {
   context('creates new project with details', () => {
     it('add new', () => {
       // //log in and assert
-      // cy.visit(Cypress.config().baseUrl)
-      // cy.logInCmd('admin', 'admin').type('{enter}')
-      // cy.url().should('eq', Cypress.config().baseUrl + 'admin/dashboard')
       cy.logInAsAdmin()
       //navigate to project
       cy.navigateTo('project')
@@ -49,13 +46,26 @@ describe('project360 - project tab functionalities', () => {
         // cy.url().should('contain', 'admin/project')
         //click modify
         cy.get('button[data-test-id="actMod"]').last().click()
+        //get projectID
+        cy.url().then((text) => {
+          //get project id
+          const array = text.split('/')
+          const projectID = array[array.length - 1]
+          cy.intercept('POST', `api/v1/material/project/${projectID}`).as('modifyingProject')
+          // cy.log(projectID)
+        })
+
         cy.get('div > h5').contains('Project detail').should('be.visible')
         cy.isProjectPropertiesEnabled()
         cy.get('div[role="tablist"] > a[data-test-id="material"]').click()
       })
       it('add new material details', () => {
+        let dataTest = [null,'69,420']
         cy.get('button').contains('Add new').click()
-        // cy.clickAddNewButton()
+        //click save right away -> should fail
+        cy.get('button[data-test-id="saveBtn"]').click()
+        //assert api res code = 500
+        cy.wait('@modifyingProject').its('response.statusCode').should('be.oneOf', [500])
         //get the master div contain 7 input tag
         //loop thru each input tag and type in value
         cy.get('div[data-test-id="projectMatDetail"] input').each(($el, index, $list) => {
@@ -68,22 +78,49 @@ describe('project360 - project tab functionalities', () => {
               .click()
           } else {
             //at other element -> type
-            cy.wrap($el).type('69420')
+            cy.wrap($el).type(dataTest[1])
             // cy.get('div[role=presentation] ul[role=listbox] li[role=option]').should('not.be.empty')
           }
         })
-        //assertion
+        //assert data entered
         cy.get('div[data-test-id="projectMatDetail"] input').each(($el, index, $list) => {
           if (index == 0) {
+            // let firstValue = cy.wrap($el).invoke('val')
+            // dataTest[0] = cy.wrap($el).invoke('val').toString()
+            // cy.log(dataTest[0])
             cy.wrap($el).invoke('val').should('not.be.empty')
           } else {
-            cy.wrap($el).invoke('val').should('not.be.empty')
+            cy.wrap($el).invoke('val').should('eq',dataTest[1])
+            // cy.wrap($el).should('eq',dataTest[1])
           }
         })
         //save
         cy.get('button[data-test-id="saveBtn"]').click()
+        cy.wait('@modifyingProject').then((interception) => {
+          assert.equal(interception.response.statusCode,200)
+          cy.log(interception.response.body.data.material.text)
+          // interception.its('response.statusCode').should('be.oneOf', [200])
+          // interception.its('response.body')
+        })
         cy.get('div[role="status"]').contains('Saved success!').should('exist').and('be.visible')
-        cy.get('div[data-testid="pagination-rows"]').click().select('100')
+        //pagination to 100
+        // cy.get('div[data-testid="pagination-rows"]').click()
+        // cy.get('ul[data-testid="pagination-menu-list"] > li[role="option"]').last().click()
+        // cy.get('div[data-testid="pagination-rows"]').should('contains','100')
+        //get the last row of material to assert UI
+        // cy.get('tr[data-test-id="row"]').last().within(() => {
+        //   cy.get('td').each(($el, index, $list) => {
+        //     if (index != 0) {
+        //       if (index == 1) {
+        //         // cy.wrap($el).should('contain',dataTest[0])
+        //         cy.wrap($el).invoke('val').should('contain','modified-name')
+        //       } else {
+        //         cy.wrap($el).invoke('val').should('contain',dataTest[1])
+        //       }
+        //     }
+        //   })
+        // })
+        
       })
       it('modify existing project', () => {
 
