6c33683de0f1919f6b9b14e43195053df0924863
masz77 <duong.buinhat1809@gmail.com>
2022-07-03 20:57:51 +0700

    local fixing sign up

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,29 +1,29 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   video: true,
   screenshotOnRunFailure: true,
   defaultCommandTimeout: 15000,
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: "mochawesome",
   reporterOptions: {
-    reportDir: 'cypress/reports',
+    reportDir: "cypress/reports",
     charts: true,
-    reportPageTitle: 'Ghe Project Suite',
+    reportPageTitle: "Ghe Project Suite",
     embeddedScreenshots: true,
     inlineAssets: true,
-    html: true,
+    html: false,
     json: false,
   },
   viewportWidth: 1280,
   viewportHeight: 720,
-  projectId: 'xbway2',
+  projectId: "xbway2",
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: 'http://ghe-staging.b2ssolution.com/',
-    specPattern: 'cypress/ghe-project-e2e-testing/*.{js,jsx,ts,tsx}',
+    baseUrl: "http://ghe-staging.b2ssolution.com/",
+    specPattern: "cypress/ghe-project-e2e-testing/*.{js,jsx,ts,tsx}",
   },
-})
+});
